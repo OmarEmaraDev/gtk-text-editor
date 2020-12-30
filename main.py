5b79f93253470d70690cc595aa723f3d5376bdad
MohamedAlkhateb <alkhateb118@gmail.com>
2020-12-30 16:38:50 +0200

    Create a FontChooserDialog and add setFont function

--- main.py
+++ main.py
@@ -37,6 +37,13 @@ class EditorWindow(Gtk.Window):
         )
         toolBar.insert(boldButton, -1)
 
+        toolBar.insert(Gtk.SeparatorToolItem(), -1)
+        
+        toolButton = Gtk.ToolButton()
+        toolButton.set_icon_name("preferences-desktop-font")
+        toolButton.connect("clicked", self.setFont, textBuffer)
+        toolBar.insert(toolButton, -1)
+
         return toolBar
 
     def saveBuffer(self, widget, textBuffer):
@@ -58,6 +65,26 @@ class EditorWindow(Gtk.Window):
             outputFile.write(serial)
 
         saveDialog.destroy()
+
+    def setFont(self, widget, textBuffer):
+        fontChooserDialog = Gtk.FontChooserDialog()
+        fontChooserDialog.set_title("FontChooserDialog")
+        response = fontChooserDialog.run()
+
+        if response != Gtk.ResponseType.OK:
+            fontChooserDialog.destroy()
+            return
+
+        fontDescriptor = fontChooserDialog.get_font_desc()
+        tagTable = textBuffer.get_tag_table()
+        tagName = fontDescriptor.to_string()
+        tag = tagTable.lookup(tagName)
+        if tag is None:
+            tag = textBuffer.create_tag(tagName, font_desc=fontDescriptor)
+        textBuffer.apply_tag(tag, *textBuffer.get_selection_bounds())
+        fontChooserDialog.destroy()
+
+
 
 editorWindow = EditorWindow()
 editorWindow.connect("destroy", Gtk.main_quit)
